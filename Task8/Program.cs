1a9911b3c1e94bbd0f48bd313c3afae87ba5630e
Marina Vystavkina <vistavkinamm@mail.ru>
2022-09-03 13:30:19 +0300

    Task_8

---
+++ Task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number=Convert.ToInt32(Console.ReadLine());
+int numberEven=2;
+
+while (numberEven<=number)
+{
+   Console.WriteLine(numberEven);
+   numberEven=numberEven+2;
+}
+
