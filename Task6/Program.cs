e958ef9751fc5e9a858323e622031e74d4b3e818
Marina Vystavkina <vistavkinamm@mail.ru>
2022-09-03 13:15:46 +0300

    Task_6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number=Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 ==0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+  Console.WriteLine("Нечетное");  
+}
