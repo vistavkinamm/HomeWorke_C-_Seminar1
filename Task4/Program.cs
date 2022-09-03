1b807a24970345c3b398ba829740246054efc88b
Marina Vystavkina <vistavkinamm@mail.ru>
2022-09-03 13:08:26 +0300

    Task_4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число 1");
+int number1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int number2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3");
+int number3=Convert.ToInt32(Console.ReadLine());
+int max=number1;
+
+if (number2>max) max=number2;
+if (number3>max) max=number3;
+
+Console.Write("max=");
+Console.WriteLine(max);
