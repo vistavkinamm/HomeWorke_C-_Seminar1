a0cc2d52f16f9b027a713f1f1455f0d8c6c16f3a
Marina Vystavkina <vistavkinamm@mail.ru>
2022-09-03 13:59:36 +0300

    Resize_Task_2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,11 +1,23 @@
 ﻿Console.WriteLine("Введите число 1");
 int number1=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число 1");
+Console.WriteLine("Введите число 2");
 int number2=Convert.ToInt32(Console.ReadLine());
 int max=number1;
+int min=number2;
 
-if (number2>number1) max=number2;
+if (number1>number2) 
+{
+    max=number1;
+    min=number2;
+}
+if (number2>number1) 
+{
+    max=number2;
+    min=number1;
+}
 
 Console.Write("max=");
-Console.Write(max);
+Console.WriteLine(max);
+Console.Write("min=");
+Console.WriteLine(min);
 
